9164faf7ed9bef7e5d11994b700de8c8354494f8
thewayforged <thewayforged@gmail.com>
2026-02-22 23:13:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tardis-v7.517'; // ← bump this number each time you deploy
+const CACHE = 'tardis-v7.518'; // ← bump this number each time you deploy
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
